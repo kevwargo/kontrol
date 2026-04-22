b35c6ef27ff4890583ce6c38c75c9b7965bd99f1
kevwargo <jarasz.kevwargo@gmail.com>
2026-04-22 20:32:36 +0300

    Fix TypeError

--- kwinctl/kwinctl.js
+++ kwinctl/kwinctl.js
@@ -4,36 +4,6 @@
   DBUS_NAME: string;
 */
 
-function kwinctlRegister(id, rule) {
-  print(`kwinctl: binding ${rule.key} to ${id}`);
-  registerShortcut(`kwinctl_${id}`, `Focus ${id} (KWinCTL)`, rule.key, () => {
-    const found = workspace
-      .windowList()
-      .find((w) => w.resourceClass === rule.cls);
-    if (found) {
-      print(
-        `kwinctl: found ${id}, switching from ${workspace.activeWindow} to ${found}`,
-      );
-      workspace.activeWindow = found;
-    } else if (rule.command) {
-      if (rule.auto) {
-        print(`kwinctl: ${id} not found, calling command ${rule.command}`);
-        callDBus(DBUS_NAME, "/", DBUS_NAME, "Execute", rule.command);
-      } else {
-        print(`kwinctl: ${id} not found, querying ${rule.command}`);
-        callDBus("org.kde.krunner", "/App", "org.kde.krunner.App", "display");
-        callDBus(
-          "org.kde.krunner",
-          "/App",
-          "org.kde.krunner.App",
-          "query",
-          rule.command,
-        );
-      }
-    }
-  });
-}
-
 const rulesByWindowId = {};
 
 function triggerRule({ id, key, candidates, command, auto }) {
@@ -104,13 +74,14 @@ function onWindowRemove(window) {
 function onWindowActivate(window) {}
 
 const wfmt = (w) => `${w.resourceName}(${w.caption})${w.internalId}`;
-const wsfmt = (ws) => `[${ws.map(wfmt).join("; ")}]`;
+const wsfmt = (ws) => `[${(ws ?? []).map(wfmt).join("; ")}]`;
 
-RULES.forEach((r) =>
+RULES.forEach((r) => {
+  print(`kwinctl: binding ${r.key} to ${JSON.stringify(r)}`);
   registerShortcut(`kwinctl_${r.id}`, `Focus ${r.id} (KWinCTL)`, r.key, () =>
     triggerRule(r),
-  ),
-);
+  );
+});
 
 workspace.windowList().forEach(onNewWindow);
 
