436e32f93de5eb3c7ed6f630b12c28eee762be65
kevwargo <jarasz.kevwargo@gmail.com>
2026-04-23 02:29:28 +0300

    Add a safeguard to kscreen-toggle-laptop

--- kscreen-toggle-laptop.sh
+++ kscreen-toggle-laptop.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-enabled=$(kscreen-doctor -j | jq -r '.outputs[]|select(.name=="eDP-1").enabled')
-if [ "$enabled" = "true" ]; then
-    kscreen-doctor output.eDP-1.disable
+if [ "$(kscreen-doctor -j | jq '.outputs|length')" -gt 1 ]; then
+    enabled=$(kscreen-doctor -j | jq -r '.outputs[]|select(.name=="eDP-1").enabled')
+    if [ "$enabled" = "true" ]; then
+        kscreen-doctor output.eDP-1.disable
+    else
+        kscreen-doctor output.eDP-1.enable
+    fi
 else
-    kscreen-doctor output.eDP-1.enable
+    echo "No external monitor"
 fi
