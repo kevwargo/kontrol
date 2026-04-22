ac0b96992d762164f6d651bd42bbb61434e32f94
kevwargo <jarasz.kevwargo@gmail.com>
2026-04-23 01:30:57 +0300

    Save KWin focus stealing prevention setting

--- configs/rekonfigista.sh
+++ configs/rekonfigista.sh
@@ -6,6 +6,9 @@ kwriteconfig6 --file kxkbrc --group Layout --key ResetOldOptions true
 kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Window to Next Screen" "Meta+Shift+Down,Meta+Shift+Right,Move Window to Next Screen"
 kwriteconfig6 --file kglobalshortcutsrc --group kwin --key "Window to Previous Screen" "Meta+Shift+Up,Meta+Shift+Left,Move Window to Previous Screen"
 
+kwriteconfig6 --file kwinrc --group Windows --key FocusStealingPreventionLevel 0
+busctl --user call org.kde.KWin /KWin org.kde.KWin reconfigure # this is proved to actually work (reload and apply config)
+
 pacman -S keyd
 cat > /etc/keyd/default.conf <<EOF
 [ids]
