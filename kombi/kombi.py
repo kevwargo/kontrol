2f9ae8e7766953fd7f2fe6a58a2128477035af52
kevwargo <jarasz.kevwargo@gmail.com>
2026-05-02 12:23:03 +0300

    Implement simple shortcut checker - kombi

---
+++ kombi/kombi.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import asyncio
+import json
+import sys
+
+from dbus_next import BusType
+from dbus_next.aio import MessageBus
+from PyQt6.QtGui import QKeySequence
+
+
+async def main(key: str):
+    qk = QKeySequence(key)
+    if not qk.toString():
+        raise ValueError(f"invalid key {key}")
+
+    bus = await MessageBus(bus_type=BusType.SESSION).connect()
+    intro = await bus.introspect("org.kde.kglobalaccel", "/kglobalaccel")
+    iface = bus.get_proxy_object("org.kde.kglobalaccel", "/kglobalaccel", intro).get_interface(
+        "org.kde.KGlobalAccel"
+    )
+    res = await iface.call_global_shortcuts_by_key([[qk[0].toCombined(), 0, 0, 0]], [0])
+    print(json.dumps(res, indent=2))
+
+
+if __name__ == "__main__":
+    asyncio.run(main(sys.argv[1]))
