90c9b69fcea5dd1821d791ce66a09c293b0479cb
kevwargo <jarasz.kevwargo@gmail.com>
2026-07-14 02:24:12 +0200

    Implement setting default sink

--- qkbdialog/audio.py
+++ qkbdialog/audio.py
@@ -5,16 +5,19 @@ import json
 import logging
 import re
 import sys
+from collections import defaultdict
+from collections.abc import Callable
 from functools import cached_property
 from signal import SIGINT
 from subprocess import PIPE, Popen
+from subprocess import run as run_cmd
 
 from dbus_next import BusType
 from dbus_next.aio import MessageBus
 from PyQt6.QtCore import QObject, QProcess, Qt, QTimer
 from PyQt6.QtGui import QKeySequence, QShortcut
-from PyQt6.QtWidgets import (QApplication, QButtonGroup, QRadioButton,
-                             QVBoxLayout, QWidget)
+from PyQt6.QtWidgets import (QApplication, QButtonGroup, QGridLayout, QLabel,
+                             QRadioButton, QWidget)
 from qasync import QEventLoop
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(message)s")
@@ -47,28 +50,11 @@ class Sink:
 class BTDevice:
     def __init__(self, dbus_path: str, mac: str, name: str | None):
         self.id = dbus_path
-        self.mac = mac
+        self.mac = mac.replace(":", "_").upper()
         self.name = name
 
     def __str__(self):
         return f"BTDev<{self.mac}({self.name!r})>"
-
-
-class AudioOutput:
-    def __init__(self, sink: Sink | None = None, bt_dev: BTDevice | None = None):
-        self.sink = sink
-        self.bt_dev = bt_dev
-        self.is_default = False
-
-    @property
-    def id(self) -> str:
-        if self.bt_dev:
-            return f"bt:{self.bt_dev.mac}"
-
-        return f"sink:{self.sink.name}"
-
-    def __str__(self):
-        return f"AudioOutput<sink={self.sink} bt_dev={self.bt_dev}>"
 
 
 class BTManager:
@@ -76,7 +62,7 @@ class BTManager:
         self.bus = bus
 
         self._event = asyncio.Event()
-        self._devices_dedup = set()
+        self._ifaces: dict[str, set] = defaultdict(set)
 
     async def start(self):
         root_intro = await self.bus.introspect("org.bluez", "/")
@@ -84,7 +70,7 @@ class BTManager:
             "org.freedesktop.DBus.ObjectManager"
         )
         manager.on_interfaces_added(self.iface_added)
-        manager.on_interfaces_removed(lambda p, ii: log(f"dbus removed: {p}({sorted(ii)})"))
+        manager.on_interfaces_removed(self.iface_removed)
 
         objects = await manager.call_get_managed_objects()
         for path, obj_ifaces in objects.items():
@@ -99,19 +85,28 @@ class BTManager:
             name = name_var.value
 
         self.on_new_device(BTDevice(dbus_path=path, mac=dev["Address"].value, name=name))
-        self._devices_dedup.add(path)
 
     def notify_adapter(self, adapter):
-        log(f"New adapter: {adapter}")
+        log(f"New adapter: {adapter['Address']}")
         self.on_adapter_state_change(True)
 
     def iface_added(self, path: str, obj_ifaces: dict):
         log(f"dbus added: {path}({sorted(obj_ifaces)})")
         if dev := obj_ifaces.get("org.bluez.Device1"):
-            if path not in self._devices_dedup:
+            if path not in self._ifaces:
                 self.notify_new_device(path, dev)
         elif adapter := obj_ifaces.get("org.bluez.Adapter1"):
             self.notify_adapter(adapter)
+
+        self._ifaces[path].update(obj_ifaces)
+
+    def iface_removed(self, path: str, obj_ifaces):
+        self._ifaces[path].difference_update(obj_ifaces)
+        if new := self._ifaces[path]:
+            log(f"dbus: some interfaces removed: {path}({sorted(obj_ifaces)}): {sorted(new)}")
+        else:
+            log(f"dbus removed completely: {path}")
+            del self._ifaces[path]
 
     async def activate_adapter(self):
         p = await asyncio.create_subprocess_exec("rfkill", ["unblock", "bluetooth"])
@@ -182,39 +177,109 @@ class SinkManager(QObject):
 
         self.on_sinks_changed(added, removed, default_sink)
 
-    def on_sinks_changed(self, added: list[Sink], removed: set[str], default: str | None): ...
+    def on_sinks_changed(self, added: list[Sink], removed: set[str], new_default: str | None): ...
 
 
-class AudioOutputRow(QRadioButton):
+class Keymap:
+    def __init__(self, parent: QWidget):
+        self._parent = parent
+        self._shortcuts: dict[str, QShortcut | None] = {
+            c: None
+            for r in (("A", "Z"), ("0", "9"))
+            for c in map(chr, range(ord(r[0]), ord(r[1]) + 1))
+        }
+
+    def bind(self, action: Callable[[], None], key: str, /, force=False):
+        if self._shortcuts[key] is not None:
+            if force:
+                self.unbind(key)
+            else:
+                raise ValueError(f"Shortcut {key!r} is already bound")
+
+        self._bind(key, action)
+
+    def bind_available(self, action: Callable[[], None]) -> str:
+        for k in self._shortcuts:
+            if self._shortcuts[k] is None:
+                log(f"Found free shortcut key: {k!r}")
+                self._bind(k, action)
+
+                return k
+
+        raise ValueError("All available shortcuts are in use")
+
+    def unbind(self, key: str):
+        if key not in self._shortcuts:
+            return
+
+        if s := self._shortcuts[key]:
+            log(f"Unbinding {key!r} (deleting {s})")
+            s.deleteLater()
+            self._shortcuts[key] = None
+
+    def _bind(self, key: str, action: Callable[[], None]):
+        s = self._shortcuts[key] = QShortcut(QKeySequence(key), self._parent)
+        log(f"Binding {key!r} to {action}: {s}")
+        s.setContext(Qt.ShortcutContext.WindowShortcut)
+        s.activated.connect(action)
+
+
+class AudioOutput(QWidget):
     def __init__(
-        self,
-        parent: QWidget,
-        bt_mgr: BTManager,
-        key: str,
-        *,
-        sink: Sink | None = None,
-        bt_dev: BTDevice | None = None,
+        self, parent: QWidget, *, sink: Sink | None = None, bt_dev: BTDevice | None = None
     ):
         if not (sink or bt_dev):
             raise ValueError(
                 f"At least one of `sink` or `bt_dev` must be specified for {type(self).__name__}"
             )
 
+        super().__init__(parent)
+
         self.sink = sink
         self.bt_dev = bt_dev
-        self.bt_mgr = bt_mgr
+        self.key: str | None = None
+
+        self.button = QRadioButton(self._name, self)
+        self._label = QLabel(self)
+
+    def match_sink(self, sink: Sink) -> bool:
+        if self.sink:
+            return self.sink.name == sink.name
+
+        if self.bt_dev:
+            return self.bt_dev.mac in sink.name.upper()
+
+        return False
+
+    def match_bt(self, bt_dev: BTDevice) -> bool:
+        if self.bt_dev:
+            return self.bt_dev.mac == bt_dev.mac
+
+        if self.sink:
+            return bt_dev.mac in self.sink.name.upper()
+
+        return False
+
+    def add_to_grid(self, grid: QGridLayout, row: int):
+        grid.addWidget(self._label, row, 0)
+        grid.addWidget(self.button, row, 1)
+
+    def set_key(self, key: str):
         self.key = key
+        self._label.setText(f"[{key}]")
 
-        super().__init__(parent=parent, text=f"({key}) {self._label()}")
-
-    def _label(self) -> str:
+    @cached_property
+    def _name(self) -> str:
         if self.bt_dev:
             return self.bt_dev.name
 
         return self.sink.description
 
     def __str__(self):
-        return f"AudioOutputRow<sink={self.sink} bt_dev={self.bt_dev}>"
+        return f"AudioOutput<sink={self.sink} bt_dev={self.bt_dev}>"
+
+    def __repr__(self):
+        return str(self)
 
     def __lt__(self, o):
         if not isinstance(o, type(self)):
@@ -223,7 +288,7 @@ class AudioOutputRow(QRadioButton):
         if (self_bt := bool(self.bt_dev)) != (o_bt := bool(o.bt_dev)):
             return self_bt < o_bt
 
-        return self._label() < o._label()
+        return self._name < o._name
 
 
 class MenuDialog(QWidget):
@@ -235,12 +300,10 @@ class MenuDialog(QWidget):
         self.setWindowTitle("Choose audio output")
         self.setWindowFlag(Qt.WindowType.Dialog)
 
-        self.layout = QVBoxLayout(self)
+        self.layout = QGridLayout(self)
         self.button_group = QButtonGroup(self)
-        self.audio_outputs: list[AudioOutputRow] = []
-        self.unused_shortcuts = [
-            c for (b, e) in (("A", "Z"), ("0", "9")) for c in map(chr, range(ord(b), ord(e) + 1))
-        ]
+        self.audio_outputs: list[AudioOutput] = []
+        self.keymap = Keymap(self)
 
         self.sink_mgr = SinkManager(self)
         self.sysbus: MessageBus | None = None
@@ -249,114 +312,107 @@ class MenuDialog(QWidget):
         self.done_event = asyncio.Event()
 
     async def run(self):
-        asyncio.get_running_loop().add_signal_handler(SIGINT, self.on_exit)
+        asyncio.get_running_loop().add_signal_handler(SIGINT, self.done_event.set)
 
-        self.sink_mgr.on_sinks_changed = self.on_sinks_changed
-        self.sink_mgr.start()
+        try:
+            self.sink_mgr.on_sinks_changed = self.on_sinks_changed
+            self.sink_mgr.start()
 
-        self.sysbus = await MessageBus(bus_type=BusType.SYSTEM).connect()
-        self.bt_mgr = BTManager(self.sysbus)
-        self.bt_mgr.on_new_device = self.on_new_bt
-        await self.bt_mgr.start()
+            self.sysbus = await MessageBus(bus_type=BusType.SYSTEM).connect()
+            self.bt_mgr = BTManager(self.sysbus)
+            self.bt_mgr.on_new_device = self.on_new_bt
+            await self.bt_mgr.start()
 
-        self.show()
+            self.keymap.bind(self.done_event.set, "Q", force=True)
 
-        await self.done_event.wait()
+            self.show()
 
-    def on_sinks_changed(self, added: list[Sink], removed: set[str], default: str | None):
+            await self.done_event.wait()
+        finally:
+            log("Cleanup...")
+
+            self.sink_mgr.stop()
+
+            if self.sysbus:
+                self.sysbus.disconnect()
+                log(f"Disconnected {self.sysbus}")
+
+    def on_sinks_changed(self, added: list[Sink], removed: set[str], new_default: str | None):
         for o in list(self.audio_outputs):
             if o.bt_dev is None and o.sink.name in removed:
+                self.keymap.unbind(o.key)
                 o.deleteLater()
-                self.unused_shortcuts.insert(0, o.key)
-                log(f"Freeing (kinda) shortcut {o.key!r}")
                 self.audio_outputs.remove(o)
 
         for sink in added:
-            if not self._update_existing_output(sink):
-                self._add_row(sink=sink)
+            if not self._assign_sink(sink):
+                self._add_output(sink=sink)
 
-        if default is not None:
+        if new_default is not None:
             for o in self.audio_outputs:
-                if o.sink and o.sink.name == default:
-                    o.setChecked(True)
+                if o.sink and o.sink.name == new_default:
+                    o.button.setChecked(True)
 
         self._update_ui()
 
-    def on_new_bt(self, dev: BTDevice):
-        # TODO: fix extremely unlikely scenario, where sink is already present
-        # when adding new BTDevice
+    def on_new_bt(self, bt_dev: BTDevice):
         for o in self.audio_outputs:
-            if o.bt_dev and o.bt_dev.mac == dev.mac:
-                log(f"Ignoring {dev} existing as {o}")
+            if o.match_bt(bt_dev):
+                o.bt_dev = bt_dev
                 return
 
-        self._add_row(bt_dev=dev)
+        self._add_output(bt_dev=bt_dev)
         self._update_ui()
 
     def _update_ui(self):
         self.audio_outputs.sort()
 
+        for o in self.audio_outputs:
+            self.keymap.unbind(o.key)
+
         while self.layout.count():
             self.layout.takeAt(0)
-        for row in self.audio_outputs:
-            self.layout.addWidget(row)
+
+        for row, o in enumerate(self.audio_outputs):
+            key = self.keymap.bind_available(o.button.animateClick)
+            o.set_key(key)
+            o.add_to_grid(self.layout, row)
 
         log("Update UI finished")
 
-    def _add_row(self, *, sink: Sink | None = None, bt_dev: BTDevice | None = None):
-        key = self.unused_shortcuts.pop(0)
-        row = AudioOutputRow(self, self.bt_mgr, key, sink=sink, bt_dev=bt_dev)
+    def _add_output(self, *, sink: Sink | None = None, bt_dev: BTDevice | None = None):
+        o = AudioOutput(self, sink=sink, bt_dev=bt_dev)
+        o.button.clicked.connect(lambda checked=False: self._on_output_clicked(o, checked))
 
-        self.button_group.addButton(row)
+        self.button_group.addButton(o.button)
+        self.audio_outputs.append(o)
 
-        log(f"Grabbing shortcut {key!r}")
-        s = QShortcut(QKeySequence(key), row)
-        s.activated.connect(row.animateClick)
-        row.toggled.connect(lambda checked: self._on_row_toggled(row, checked))
+        log(f"Added to UI: {o}")
 
-        self.audio_outputs.append(row)
+    def _on_output_clicked(self, o: AudioOutput, checked: bool):
+        log(f"{'Checked' if checked else 'Unchecked'} {o}")
+        if o.sink:
+            run_cmd(["pactl", "set-default-sink", o.sink.name], check=True)
+        else:
+            log(f"{o} does not have a sink")
 
-        log(f"Added to UI: {row}")
-
-    def _on_row_toggled(self, row: AudioOutputRow, checked: bool):
-        log(f"{row} checked:{checked}")
-
-    def _update_existing_output(self, sink: Sink) -> bool:
+    def _enable_outputs(self):
+        log("Enabling outputs")
         for o in self.audio_outputs:
-            if o.sink and sink.name == o.sink.name:
-                log(f"{o} matched by sink name for {sink}")
-            elif o.bt_dev and o.bt_dev.mac.replace(":", "_").upper() in sink.name.upper():
-                log(f"{o} matched by BT dev MAC for {sink}")
-            else:
-                continue
+            o.button.setDisabled(False)
 
-            o.sink = sink
-
-            return True
+    def _assign_sink(self, sink: Sink) -> bool:
+        for o in self.audio_outputs:
+            if o.match_sink(sink):
+                o.sink = sink
+                return True
 
         return False
 
     def closeEvent(self, ev):
         log(f"CloseEvent: {ev}")
-        self.on_exit()
         ev.accept()
-
-    def on_exit(self):
-        log("Cleanup on_exit()")
-
-        self.sink_mgr.stop()
-
-        if self.sysbus:
-            self.sysbus.disconnect()
-            log(f"Disconnected {self.sysbus}")
-
         self.done_event.set()
-
-
-async def cmd_output(program: str, *args) -> bytes:
-    p = await asyncio.create_subprocess_exec(program, *args, stdout=PIPE)
-    out, _ = await p.communicate()
-    return out
 
 
 if __name__ == "__main__":
